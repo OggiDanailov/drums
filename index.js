47f19c390e8e6d8621b74dd41425e68e3ff29eeb
Oggi Danailov <ogidan@abv.bg>
2017-10-31 22:55:42 -0400

    current time added

--- index.js
+++ index.js
@@ -7,11 +7,13 @@
 	box.style.height = '200px'
 	box.style.border = '5px solid red'
 
-	
 
+	
+var timeLine = [];
 for(let i=0;i< 16;i++){
 	kiq = document.createElement('DIV')
 	kiq.id = 'r' + i
+	timeLine.push(kiq)
 	kiq.style.width = '30px'
 	kiq.style.height = '30px'
 	// kiq.style.border = '2px solid red'
@@ -19,11 +21,14 @@ for(let i=0;i< 16;i++){
 	kiq.style.backgroundColor = 'red'
 	kiq.style.margin = '5px'
 	kiq.style.float = 'left'
+	
 	box.appendChild(kiq)
+
 }
 
 
-var timeLine = 16;
+
+
 var currentStep = 0;
 var currentSpeed = 200
 
@@ -33,8 +38,13 @@ playButton.addEventListener('click', start)
 function start(){
 	setInterval(() => {
 		currentStep += 1;
-		kiq.id = 'r' + currentStep;
-		console.log(kiq.id)
+		currentKiq = 0;
+		if(currentKiq = timeLine[currentStep - 1]){
+			currentKiq.style.backgroundColor = 'green'
+		}else {
+			currentKiq.style.backgroundColor = 'red'
+		}
+		
 		if(currentStep >= 16){
 			currentStep =1;
 		}
